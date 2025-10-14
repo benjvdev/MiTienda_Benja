33f1f6dfb7ada752ceb5c1a6dab69a2bc514f0b3
Melipilla <melipilla@duoc.acad>
2025-10-14 18:36:32 -0300

    le copio todo al profe y me a pela

--- app/src/main/java/com/example/teacherstore/viewmodel/EstadoViewModel.kt
+++ app/src/main/java/com/example/teacherstore/viewmodel/EstadoViewModel.kt
@@ -1,2 +1,6 @@
 package com.example.teacherstore.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class EstadoViewModel(application: Application) : AndroidViewModel(application)
