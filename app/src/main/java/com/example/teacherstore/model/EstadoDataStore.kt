0a8ce6656961bbb3cebc40500d2509beb97d69ad
Melipilla <melipilla@duoc.acad>
2025-10-14 18:34:13 -0300

    le copio todo al profe y me la pela

--- app/src/main/java/com/example/teacherstore/model/EstadoDataStore.kt
+++ app/src/main/java/com/example/teacherstore/model/EstadoDataStore.kt
@@ -1,6 +1,30 @@
 package com.example.teacherstore.model
 
 import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 val Context.dataStore by preferencesDataStore("preferencias_usuario")
+
+class EstadoDataStore(private val context: Context){
+
+    private val ESTADO_ACTIVADO = booleanPreferencesKey("modo activado")
+
+    suspend fun guardarEstado(nuevoEstado: Boolean){
+        context.dataStore.edit { preferencias ->
+            preferencias[ESTADO_ACTIVADO] = nuevoEstado
+        }
+    }
+
+    suspend fun obtenerEstado(): Flow<Boolean?>{
+        return context.dataStore.data.map { preferencias ->
+            preferencias[ESTADO_ACTIVADO]
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/teacherstore/viewmodel/EstadoViewModel.kt
@@ -0,0 +1,2 @@
+package com.example.teacherstore.viewmodel
+
